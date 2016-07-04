f60b468db659ac668396c671fa6561b8bae1971f
ob00042 <ob00042@gmail.com>
2016-07-04 22:24:49 +0300

    Project done!

--- simple_server.rb
+++ simple_server.rb
@@ -1,21 +1,36 @@
 require "socket"
+require "json"
 
 server = TCPServer.open(2000)
 loop {
 	client = server.accept
-	parts = client.gets.split(" ")
-	if parts[0] == "GET"
-	  if parts[1] == "/index.html"
-	  	file = File.open("index.html", "r")
+	response = client.read_nonblock(256)
+	headers, body = response.split("\r\n\r\n")
+	
+	header_parts = headers.split(" ")
+	method = header_parts[0]
+	path = header_parts[1][1..-1]
+
+	if File.exist?(path)
+	  client.puts "HTTP/1.0 200 OK\r\n#{Time.now.ctime}\r\nContent-Type: text/html\r\n\r\n"
+	  if method == "GET"
+	  	file = File.open(path, "r")
 	  	contents = file.read
-	  	client.puts "HTTP/1.0 200 OK"
-	  	client.puts(Time.now.ctime)
-	  	client.puts "Content-Type: text/html"
-	  	client.puts "Content-Length: #{contents.size}\r\n\r\n"
-	  	client.puts(contents)
-	  	client.close
+	    client.puts "Content-Length: #{contents.size}\r\n\r\n"
+	    client.puts (contents)
+	    client.close
+	  elsif method == "POST"
+	    params = JSON.parse body
+	    user_data = "<li>name: #{params["viking"]["name"]}</li><li>email: #{params["viking"]["email"]}</li>"
+	    file = File.open(path, "r")
+	    contents = file.read
+	    client.puts contents.gsub("<%= yield %>", user_data)
+	    client.close
 	  end
 	else
 	  client.puts "ERROR"
+	  client.close
 	end
+
+	
 }
--- tiny_web_browser.rb
+++ tiny_web_browser.rb
@@ -1,14 +1,45 @@
 require "socket"
+require 'json'
 
 host = "localhost"
 port = 2000
 path = "/index.html"
+puts "What type of request you want to make? (GET/POST)"
+req = gets.chomp
 
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+if req == "GET"
+  request = "GET #{path} HTTP/1.0\r\n\r\n"
 
-socket = TCPSocket.open(host, port)
-socket.print(request)
-response = socket.read
+  socket = TCPSocket.open(host, port)
+  socket.print(request)
+  response = socket.read
 
-headers, body = response.split("\r\n\r\n", 2)
-print body
+  headers, body = response.split("\r\n\r\n", 2)
+  print body
+  socket.close
+
+elsif req == "POST"
+
+  puts "We are registering for a viking raid!"
+  print "Enter your name to take part: "
+  name = gets.chomp
+  print "Enter your email to take part: "
+  email = gets.chomp
+  hash = { :viking => {:name => name, :email => email} }
+  json_hash = hash.to_json
+
+  socket = TCPSocket.open(host, port)
+  socket.puts "POST /thanks.html HTTP/1.0\r\nFrom: ob00042@gmail.com\r\nUser-Agent: HTTPTool/1.0\r\nContent-Type: application/...\r\nContent-Length: #{json_hash.length}\r\n\r\n#{json_hash}"
+
+  response = socket.read
+  headers, body = response.split("\r\n\r\n", 2)
+  print body
+  socket.close
+
+else
+
+  socket = TCPSocket.open(host, port)
+  socket.puts "ERROR"
+  socket.close
+
+end
