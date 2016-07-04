b908ca80c2b8da80346a28a3e2d02f49730beb38
ob00042 <ob00042@gmail.com>
2016-07-04 14:57:05 +0300

    server and browser work with html.S Step 10

--- simple_server.rb
+++ simple_server.rb
@@ -3,7 +3,19 @@ require "socket"
 server = TCPServer.open(2000)
 loop {
 	client = server.accept
-	client.puts(Time.now.ctime)
-	client.puts "Closing the connection. Bye!"
-	client.close
+	parts = client.gets.split(" ")
+	if parts[0] == "GET"
+	  if parts[1] == "/index.html"
+	  	file = File.open("index.html", "r")
+	  	contents = file.read
+	  	client.puts "HTTP/1.0 200 OK"
+	  	client.puts(Time.now.ctime)
+	  	client.puts "Content-Type: text/html"
+	  	client.puts "Content-Length: #{contents.size}\r\n\r\n"
+	  	client.puts(contents)
+	  	client.close
+	  end
+	else
+	  client.puts "ERROR"
+	end
 }
